8e6e018b25c83245b17107afa9989113f05bca79
Gerasin <2775748@gmail.com>
2016-02-19 18:29:13 +0300

    табы

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
 
-	console.log('pashet');
 	$('.additional-services-h a').click(function(){
 		$(this).toggleClass('active');
 		$('.additional-services-cont').slideToggle();
 		return false;
 	});
+
+	$('.tabs-item:first').show();
+	$('.list-tabs a').click(function(){
+		var index = $(this).parent().index();
+		$(this).parents('.list-tabs').find('li').removeClass('active');
+		$(this).parents().addClass('active');
+		$('.tabs-item').hide();
+		$('.tabs-item').eq(index).show();
+		return false;
+	});
+
+	$('.bank-tarif-h').click(function(){
+		$(this).toggleClass('active');
+		$(this).next('.bank-tarif-cont').slideToggle();
+	});
+
+
 
 });
